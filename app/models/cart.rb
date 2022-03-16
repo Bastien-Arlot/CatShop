e8267ad5b37376f0a49d1c55225dfb1aee4e4880
guillaume <reygner0224@gmail.com>
2022-03-16 12:24:52 +0100

    add order controller + maj cart controller

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 class Cart < ApplicationRecord
   # before_action :authenticate_user!, only: [: rajouter des pages]
   
-  has_many :join_table_cart_items
+  has_many :join_table_cart_items, dependent: :destroy
   has_many :items, through: :join_table_cart_items
   
   belongs_to :user
